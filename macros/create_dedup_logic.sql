b7af8726424359d2acaa7718e7a91e56cf28d7ac
Oded Geron <oded@graphiti.ai>
2023-12-13 09:38:05 +0000

    dedup logic test

---
+++ macros/create_dedup_logic.sql
@@ -0,0 +1,16 @@
+-- deduplication_macro.sql
+
+{% macro dedup_logic(primary_key, cursor_field, model_name) %}
+
+dedup_cte AS (
+  SELECT
+    {{ ref(model_name) }}.*,
+    ROW_NUMBER() OVER (PARTITION BY {{ primary_key }} ORDER BY {{ cursor_field }} DESC) AS row_num
+  FROM base
+)
+
+SELECT
+  {{ ref(model_name) }}.*,
+FROM dedup_cte
+WHERE row_num = 1;
+{% endmacro %}
