7bb313b5099899f97a8b7bb1db746b6ef594495d
Oded Geron <oded@graphiti.ai>
2023-12-14 17:17:58 +0000

    new approach

--- models/staging/stripe_graphiti_dbt/customers.sql
+++ models/staging/stripe_graphiti_dbt/customers.sql
@@ -13,18 +13,11 @@ SELECT
     _airbyte_data ->> 'updated' as updated,
     _airbyte_data ->> 'object' as object
 FROM {{source ('stripe_graphiti_dbt', '_airbyte_raw_customers')}}
-), 
-config_parmas AS (
-    SELECT
-        sync_mode,
-        primary_key,
-        cursor_field
-    FROM
-    public.dbt_model_configs
-    WHERE
-    airbyte_workspace_id = {{var('workspace_id')}}
-    AND model_name = {{ this.name }}
 )
 
+{% set primary_key = run_query("SELECT primary_key FROM public.dbt_model_configs WHERE airbyte_workspace_id = {{var('workspace_id')}} AND model_name = {{ this.name }}").columns[0].values() %}
+{% set cursor_field = run_query("SELECT cursor_field FROM public.dbt_model_configs WHERE airbyte_workspace_id = {{var('workspace_id')}} AND model_name = {{ this.name }}").columns[0].values() %}
+{% set sync_mode = run_query("SELECT sync_mode FROM public.dbt_model_configs WHERE airbyte_workspace_id = {{var('workspace_id')}} AND model_name = {{ this.name }}").columns[0].values() %}
 
-{{ dedup_logic( primary_key = (SELECT primary_key FROM config_parmas), cursor_field = (SELECT cursor_field FROM config_parmas) ) }}
+
+{{ dedup_logic( primary_key = var('primary_key'), cursor_field = var('cursor_field') ) }}
