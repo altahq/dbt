92ad9ddb8de81a0fa855cd9b07f8f303eb377102
Oded Geron <oded@graphiti.ai>
2023-12-17 08:41:12 +0000

    moved the config query

--- models/staging/stripe_graphiti_dbt/customers.sql
+++ models/staging/stripe_graphiti_dbt/customers.sql
@@ -1,5 +1,21 @@
 {{ config(enabled=true, materialized='table', dist='id', schema='stripe_graphiti_dbt') }}
 
+{% set model_config_query %}
+    SELECT 
+        primary_key, cursor_field, sync_mode
+    FROM public.dbt_model_configs 
+    WHERE 
+        airbyte_workspace_id = ' {{var('workspace_id')}} '
+        AND model_name = ' {{ this.name }} '
+{% endset %}
+
+{% set results = run_query(model_config_query) %}
+
+{% set primary_key = results.columns[0].values() %}
+{% set cursor_field = results.columns[1].values() %}
+{% set sync_mode = results.columns[2].values() %}
+
+
 WITH 
 base AS (
 SELECT
@@ -15,23 +31,5 @@ SELECT
 FROM {{source ('stripe_graphiti_dbt', '_airbyte_raw_customers')}}
 ),
 
-{% set model_config_query %}
-    SELECT 
-        primary_key, cursor_field, sync_mode
-    FROM public.dbt_model_configs 
-    WHERE 
-        airbyte_workspace_id = ' {{var('workspace_id')}} '
-        AND model_name = ' {{ this.name }} '
-{% endset %}
-
-{% set results = run_query(model_config_query) %}
-
-{% if execute %}
-
-{% set primary_key = results.columns[0].values() %}
-{% set cursor_field = results.columns[1].values() %}
-{% set sync_mode = results.columns[2].values() %}
 
 {{ dedup_logic( var('primary_key')  ,  var('cursor_field')  ) }}
-
-{% endif %}
