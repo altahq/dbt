b4e2b4fb2a0282acc6d48c714a59fa51c184b302
Oded Geron <oded@graphiti.ai>
2023-12-18 13:06:40 +0000

    adding comma

--- macros/new_dedup.sql
+++ macros/new_dedup.sql
@@ -3,7 +3,7 @@
 {% macro new_dedup(primary_key, cursor_field) %}
 
 SELECT
-    DISTINCT ON ({{ primary_key }}) *
+    DISTINCT ON ({{ primary_key }}) *,
     now() AS dbt_sync_time
 FROM base
 {% if is_incremental() %}
