1fe02cac8dd08e7cf85567d0ca7a66ea39ec259c
Oded Geron <oded@graphiti.ai>
2023-12-18 12:58:59 +0000

    change dedup to distinct on

--- macros/new_dedup.sql
+++ macros/new_dedup.sql
@@ -2,16 +2,9 @@
 
 {% macro new_dedup(primary_key, cursor_field) %}
 
-dedup_cte AS (
-  SELECT
-    base.*,
-    ROW_NUMBER() OVER (PARTITION BY {{ primary_key }} ORDER BY {{ cursor_field }} DESC) AS row_num
-  FROM base
-)
-
 SELECT
-  *,
-  now() AS dbt_sync_time
-FROM dedup_cte
-WHERE row_num = 1
+    DISTINCT ON {{ primary_key }}, *
+    now() AS dbt_sync_time
+FROM base
+ORDER BY {{ primary_key }}{{ ',' ~ cursor_field }}
 {% endmacro %}
