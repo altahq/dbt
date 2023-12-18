9ebb36c1b58c5035169e85fe831bbeda03b8543a
Oded Geron <oded@graphiti.ai>
2023-12-18 10:21:13 +0000

    fixing minor bug

--- models/staging/stripe_graphiti_dbt/invoice_line_items.sql
+++ models/staging/stripe_graphiti_dbt/invoice_line_items.sql
@@ -20,6 +20,6 @@ SELECT
     _airbyte_data ->> 'type' as type,
     _airbyte_data ->> 'invoice_id' as invoice_id
 FROM {{source ('stripe_graphiti_dbt', '_airbyte_raw_invoice_line_items')}}
-),
+)
 
 {{ dedup_logic('invoice_line_items', var('workspace_id')) }}
