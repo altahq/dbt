ede1db1218f5d8e895536db08ba3230efcd2a96a
Oded Geron <oded@graphiti.ai>
2023-12-21 10:56:11 +0000

    updated hubspot schema

--- models/staging/hubspot/engagements_dbt.sql
+++ models/staging/hubspot/engagements_dbt.sql
@@ -21,15 +21,17 @@ base AS (
 SELECT
     _airbyte_data,
     _airbyte_data ->> 'id' as id,
-    _airbyte_data ->> 'createdAt' as createdat,
-    _airbyte_data ->> 'lastUpdated' as lastupdated,
+    (_airbyte_data ->> 'createdAt')::bigint as createdat,
+    (_airbyte_data ->> 'lastUpdated')::bigint as lastupdated,
     _airbyte_data ->> 'createdBy' as createdby,
-    _airbyte_data ->> 'timestamp' as timestamp,
+    (_airbyte_data ->> 'timestamp')::bigint as timestamp,
     _airbyte_data ->> 'ownerId' as ownerid,
     _airbyte_data ->> 'teamid' as teamid,
     _airbyte_data ->> 'source' as source,
     _airbyte_data -> 'attachments' as attachments,
-    _airbyte_data ->> 'type' as type
+    _airbyte_data ->> 'type' as type,
+    _airbyte_data -> 'associations_companyIds' as companies,
+    _airbyte_data -> 'associations_contactIds' as contacts
 FROM {{source ('hubspot', '_airbyte_raw_engagements')}}
 )
 
